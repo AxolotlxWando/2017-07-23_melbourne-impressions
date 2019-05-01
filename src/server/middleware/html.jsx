614cf1f4311f5e690633d703af3a8a78dcfbf743
Wesley Wang <zakudesune@gmail.com>
2019-05-01 17:52:01 +1000

    Update assets and bundle references to use public path

--- src/server/middleware/html.jsx
+++ src/server/middleware/html.jsx
@@ -16,15 +16,15 @@ const Html = ({ content, state, assetMap, css, helmet }) => {
         {helmet.link.toComponent()}
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="180x180" href={`/${assetMap["apple-touch-icon.png"]}`}/>
-        <link rel="icon" type="image/png" href={`/${assetMap["favicon-32x32.png"]}`} sizes="32x32"/>
-        <link rel="icon" type="image/png" href={`/${assetMap["favicon-16x16.png"]}`} sizes="16x16"/>
-        <link rel="manifest" href={`/${assetMap["manifest.json"]}`}/>
-        <link rel="mask-icon" href={`/${assetMap["safari-pinned-tab.svg"]}`} color="#5bbad5"/>
-        <link rel="shortcut icon" href={`/${assetMap["favicon.ico"]}`}/>
-        <meta name="msapplication-config" content={`/${assetMap["browserconfig.xml"]}`}/>
+        <link rel="apple-touch-icon" sizes="180x180" href={__PUBLIC_URL__ + `/${assetMap["apple-touch-icon.png"]}`}/>
+        <link rel="icon" type="image/png" href={__PUBLIC_URL__ + `/${assetMap["favicon-32x32.png"]}`} sizes="32x32"/>
+        <link rel="icon" type="image/png" href={__PUBLIC_URL__ + `/${assetMap["favicon-16x16.png"]}`} sizes="16x16"/>
+        <link rel="manifest" href={__PUBLIC_URL__ + `/${assetMap["manifest.json"]}`}/>
+        <link rel="mask-icon" href={__PUBLIC_URL__ + `/${assetMap["safari-pinned-tab.svg"]}`} color="#5bbad5"/>
+        <link rel="shortcut icon" href={__PUBLIC_URL__ + `/${assetMap["favicon.ico"]}`}/>
+        <meta name="msapplication-config" content={__PUBLIC_URL__ + `/${assetMap["browserconfig.xml"]}`}/>
         <meta name="theme-color" content="#ffffff"/>
-        {!__DEV__ && <link rel="stylesheet" type="text/css" href={`/${assetMap['bundle.css']}`}/>}
+        {!__DEV__ && <link rel="stylesheet" type="text/css" href={__PUBLIC_URL__ + `/${assetMap['bundle.css']}`}/>}
         {!!__DEV__ &&
         <style dangerouslySetInnerHTML={{
           __html: styles._getCss()
@@ -33,13 +33,14 @@ const Html = ({ content, state, assetMap, css, helmet }) => {
         {!!css && css}
       </head>
       <body {...bodyAttrs}>
+        <h1>{'Backend url is ' + __PUBLIC_URL__}</h1>
         <div id="content" dangerouslySetInnerHTML={{ __html: content || "" }}/>
         <script
           dangerouslySetInnerHTML={{ __html: `window.__APOLLO_STATE__=${serialize(state, { isJSON: true })};` }}
           charSet="UTF-8"
         />
-        {assetMap["vendor.js"] && <script src={`/${assetMap["vendor.js"]}`} charSet="utf-8"/>}
-        <script src={`/${assetMap['bundle.js']}`} charSet="utf-8"/>
+        {assetMap["vendor.js"] && <script src={__PUBLIC_URL__ + `/${assetMap["vendor.js"]}`} charSet="utf-8"/>}
+        <script src={__PUBLIC_URL__ + `/${assetMap['bundle.js']}`} charSet="utf-8"/>
       </body>
     </html>
   );
